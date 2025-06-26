b02a35b88819e58bb6be722b6ef3b146d7663055
José Amorim <josepedropamorim@gmail.com>
2025-06-26 22:01:37 +0000

    fix: remove verbose flag on ffmpeg and remove video and subtitles after combining

--- scripts/create_hardsubtitles.sh
+++ scripts/create_hardsubtitles.sh
@@ -10,17 +10,33 @@ if [ ! -d "$target_dir" ]; then
 fi
 
 find "$target_dir" -type f -name "*.mp4" | while read -r mp4file; do
+    echo "--------------------------------------------------------------"
+    echo "Processing file: $mp4file"
+    echo "--------------------------------------------------------------"
+    
+    # Skip files that already have "_hard" in their name
+    if [[ "$mp4file" == *"_hard"* ]]; then
+        echo "Skipping already hardsubbed file: $mp4file"
+        continue
+    fi
+
     parent_dir="$(dirname "$mp4file")"
     vttfile=$(find "$parent_dir" -maxdepth 1 -type f -name "*.vtt" | head -n 1)
+    newfile="${mp4file%.mp4}_hard.mp4"
+    echo "newfile=$newfile"
+
+    if [ -f "$newfile" ]; then
+        echo "File $newfile already exists. Skipping."
+        continue
+    fi
+
     if [ -f "$vttfile" ]; then
-        ffmpeg -i "$mp4file" -vf subtitles="$vttfile" "${mp4file%.mp4}_hard.mp4"
+        ffmpeg -loglevel error -i "$mp4file" -vf subtitles="$vttfile" "$newfile"
     else
         echo "Warning: VTT file not found for $mp4file"
     fi
-    hidden_file="$(dirname "$mp4file")/.$(basename "$mp4file")"
-    mv "$mp4file" "$hidden_file"
-    if [ -f "$vttfile" ]; then
-        hidden_vtt="$(dirname "$vttfile")/.$(basename "$vttfile")"
-        mv "$vttfile" "$hidden_vtt"
-    fi
+
+    rm "$mp4file"
+    rm "$vttfile"
+
 done
