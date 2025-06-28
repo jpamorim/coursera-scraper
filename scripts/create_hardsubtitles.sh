6c98c0b84979cba875a998e8eecd7ad55150470a
José Amorim <josepedropamorim@gmail.com>
2025-06-28 15:19:29 +0000

    fix: fixed create_hardsubtitles script by finding all directories with mp4 files

--- scripts/create_hardsubtitles.sh
+++ scripts/create_hardsubtitles.sh
@@ -2,41 +2,38 @@
 
 read -p "Enter course-id: " course_id
 
-target_dir="downloads/${course_id}"
+target_dir="downloads/${course_id}/"
 
-if [ ! -d "$target_dir" ]; then
+if [ ! -d "$target_dir" ]; then 
     echo "Directory '$target_dir' does not exist."
     exit 1
 fi
 
-find "$target_dir" -type f -name "*.mp4" | while read -r mp4file; do
-    echo "--------------------------------------------------------------"
-    echo "Processing file: $mp4file"
-    echo "--------------------------------------------------------------"
-    
-    # Skip files that already have "_hard" in their name
+find "$target_dir" -type f -name "*.mp4" -exec dirname {} \; | sort -u | while read -r dir; do
+    mp4file=$(find "$dir" -maxdepth 1 -type f -name "*.mp4" | head -n 1)
+    vttfile=$(find "$dir" -maxdepth 1 -type f -name "*.vtt" | head -n 1)
+
     if [[ "$mp4file" == *"_hard"* ]]; then
         echo "Skipping already hardsubbed file: $mp4file"
         continue
     fi
 
-    parent_dir="$(dirname "$mp4file")"
-    vttfile=$(find "$parent_dir" -maxdepth 1 -type f -name "*.vtt" | head -n 1)
     newfile="${mp4file%.mp4}_hard.mp4"
-    echo "newfile=$newfile"
 
-    if [ -f "$newfile" ]; then
-        echo "File $newfile already exists. Skipping."
-        continue
-    fi
-
+    
     if [ -f "$vttfile" ]; then
         ffmpeg -loglevel error -i "$mp4file" -vf subtitles="$vttfile" "$newfile"
     else
         echo "Warning: VTT file not found for $mp4file"
+        continue
     fi
 
-    rm "$mp4file"
-    rm "$vttfile"
+    if [ -f "$newfile" ]; then
+        echo "Succeess: File $newfile was created successfully. Removing video and subtitles."
+        rm "$mp4file"
+        rm "$vttfile"
+    else
+        echo "Warning: Hardsubtitles were not created for $mp4file"
+    fi
 
 done
